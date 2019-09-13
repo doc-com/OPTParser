fe0393b99962768ef5432859120c5dcb6015a730
Erik <erik@doc.com>
2019-09-13 17:41:19 -0500

    Parsing from AOM2 model

--- src/main/kotlin/com/doc/Hello.kt
+++ src/main/kotlin/com/doc/Hello.kt
@@ -1,10 +1,10 @@
 package com.doc
 
-import org.openehr.schemas.v1.CARCHETYPEROOT
-import org.openehr.schemas.v1.OPERATIONALTEMPLATE
+import openEHR.v1.template.COMPOSITION
+import org.openehr.schemas.v1.*
+import org.openehr.schemas.v1.impl.CMULTIPLEATTRIBUTEImpl
 import org.openehr.schemas.v1.impl.OPERATIONALTEMPLATEImpl
 import java.io.File
-
 
 fun main(args: Array<String>) {
 
@@ -12,8 +12,19 @@ fun main(args: Array<String>) {
     val base = OPERATIONALTEMPLATE.Factory.parse(file)
     val baseObject = base.selectChildren("http://schemas.openehr.org/v1", "template")[0]
     val opt = OPERATIONALTEMPLATE.Factory.parse(baseObject.toString()) as OPERATIONALTEMPLATEImpl
+    findDefinitionElements(opt.definition)
 
-    println((opt.definition as CARCHETYPEROOT))
+}
 
+fun findDefinitionElements(root: CARCHETYPEROOT) {
+    if (root.rmTypeName == COMPOSITION::class.simpleName) {
+        val content: CMULTIPLEATTRIBUTE =
+            CMULTIPLEATTRIBUTE.Factory.parse(root.attributesArray.single { cattribute: CATTRIBUTE? -> cattribute?.rmAttributeName == "content" }.toString())
+        findContentElementsRecursively(content)
+    }
+}
+
+fun findContentElementsRecursively(content: CMULTIPLEATTRIBUTE) {
+    println(content.childrenArray)
 }
 
